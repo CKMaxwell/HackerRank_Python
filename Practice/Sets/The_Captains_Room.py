2088f7bf5f23a26e10d0d418588f8448b6384ad7
CKMaxwell <39400519+CKMaxwell@users.noreply.github.com>
2020-07-07 10:49:22 +0800

    20200707

--- Practice/Sets/The_Captains_Room.py
+++ Practice/Sets/The_Captains_Room.py
@@ -1,13 +1,15 @@
-# 20200703 - The Captain's Room
+# 20200707 - The Captain's Room
 num = int(input())
 orig = list(map(int, input().split()))
-orig_set = set(orig)
-for i in orig_set:
-    count = 0
-    for j in range(len(orig)):
-        if orig[j] == i:
-            count = count + 1
-    if count == 1:
-        ans = i
-        break
-print(ans)
+# orig_set = set(orig)
+empty = set()
+second = set()
+for i in orig:
+    if i not in empty:
+        empty.add(i)
+    else:
+        second.add(i)
+for j in second:
+    empty.remove(j)
+ans = list(map(int, empty))
+print(ans[0])
